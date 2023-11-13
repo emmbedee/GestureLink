c2401aead5ced52e8f7bcbe9ef7e78d97bd2010b
Admin <mdoeden@unomaha.edu>
2023-11-13 13:13:57 -0600

    Base GUI done, still unfinished, basic funcitonality is added.  Needs polish pass and add more gestures and functionality.

--- gs_main.py
+++ gs_main.py
@@ -1,8 +1,8 @@
 import cv2
 import mediapipe as mp
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
-import threading
 
 # Initialize MediaPipe solutions for hand tracking
 mp_hands = mp.solutions.hands
@@ -42,7 +42,6 @@ def gesture_recognized(hand_landmarks):
 
     return is_index_up and are_other_fingers_down
 
-
 def perform_action():
     # [Your action performing logic here...]
     pass
@@ -53,16 +52,43 @@ class GestureLinkApp:
         self.window = window
         self.window.title(window_title)
 
-        self.video_source = 0  # default webcam
+        # Style configuration for tabs to make them larger and centered
+        style = ttk.Style()
+        style.configure('Large.TNotebook.Tab', font=('Arial', '12'), padding=[20, 8])
 
+        # Change the color of the text of the selected tab to blue
+        style.map('Large.TNotebook.Tab',
+                  foreground=[('selected', 'blue')],
+                  background=[
+                      ('selected', 'grey')])  # Optional: change the background color of the selected tab as well
+
+        # Create a tab control with the new style
+        self.tabControl = ttk.Notebook(self.window, style='Large.TNotebook')
+
+        # Create tabs
+        self.tabHome = ttk.Frame(self.tabControl)
+        self.tabGestures = ttk.Frame(self.tabControl)
+        self.tabWebcam = ttk.Frame(self.tabControl)
+        self.tabSettings = ttk.Frame(self.tabControl)
+        self.tabProfiles = ttk.Frame(self.tabControl)
+
+        # Add tabs to the tab control
+        self.tabControl.add(self.tabHome, text='Home')
+        self.tabControl.add(self.tabGestures, text='Gestures')
+        self.tabControl.add(self.tabWebcam, text='Webcam')
+        self.tabControl.add(self.tabSettings, text='Settings')
+        self.tabControl.add(self.tabProfiles, text='Profiles')
+        self.tabControl.pack(expand=1, fill="both")
+
+        # Webcam feed section
+        self.video_source = 0  # default webcam
         self.vid = cv2.VideoCapture(self.video_source)
-        self.canvas = tk.Canvas(window, width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH), height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.canvas = tk.Canvas(self.tabWebcam, width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH), height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.canvas.pack()
 
-        self.btn_text = tk.StringVar()
-        self.btn_text.set("Start Webcam")
-        self.btn_toggle_webcam = tk.Button(window, textvariable=self.btn_text, width=50, command=self.toggle_webcam)
-        self.btn_toggle_webcam.pack(anchor=tk.CENTER, expand=True)
+        # Start/Stop button
+        self.btn_toggle_webcam = tk.Button(self.tabWebcam, text="Start", width=10, command=self.toggle_webcam, bg='green')
+        self.btn_toggle_webcam.pack(pady=20)
 
         self.running = False
         self.update()
@@ -72,17 +98,17 @@ class GestureLinkApp:
     def toggle_webcam(self):
         if self.running:
             self.running = False
-            self.btn_text.set("Start Webcam")
+            self.btn_toggle_webcam.config(text="Start", bg='green')
+            self.canvas.delete("all")  # Clear the canvas
         else:
             self.running = True
-            self.btn_text.set("Stop Webcam")
+            self.btn_toggle_webcam.config(text="Stop", bg='red')
 
     def update(self):
         if self.running:
             ret, frame = self.vid.read()
             if ret:
                 self.process_frame(frame)
-
         self.window.after(10, self.update)
 
     def process_frame(self, frame):
@@ -94,7 +120,6 @@ class GestureLinkApp:
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
                 if gesture_recognized(hand_landmarks):
                     perform_action()
                     cv2.putText(frame, 'Gesture Recognized!', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
