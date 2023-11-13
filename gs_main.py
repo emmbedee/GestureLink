cb3d01921430843f59570aecaf40595b071a4e1d
Admin <mdoeden@unomaha.edu>
2023-11-13 12:08:04 -0600

    Rewrite

--- gs_main.py
+++ gs_main.py
@@ -1,77 +1,58 @@
-# The following is a ROUGH DRAFT code outline generally for what may or may not be necessary for the GS prototype.
-# This WILL change.
-#
-# List of libraries that MAY BE needed: pip install opencv-python opencv-python-headless numpy mediapipe
-
-# Capture video input.
-
 import cv2
-
-cap = cv2.VideoCapture(0)  # 0 is the default camera
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    # Process frame here
-
-    cv2.imshow('Frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-# Process video grames via MEDIAPIPE plugin (NOT INSTALLED YET).
 import mediapipe as mp
+import pyautogui
+import os
+import webbrowser
 
+# Initialize MediaPipe solutions for hand tracking
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
 
-    # Convert the BGR image to RGB
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # Process the frame and get the hand landmarks
-    results = hands.process(rgb_frame)
-
-    # Draw landmarks on the frame
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-    cv2.imshow('Frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-hands.close()
-
-def recognize_gesture(hand_landmarks):
-    # Implement gesture recognition logic here
-    # Once a gesture is recognized, translate it to the corresponding input command.
-    # Could use the pyautogui library to simulate keyboard and mouse inputs.
+def gesture_recognized(hand_landmarks):
+    # Placeholder function for gesture recognition logic
+    # Return True if a specific gesture is recognized
     pass
 
-import pyautogui # (NOT INSTALLED YET)
 
-def execute_command(gesture):
-    if gesture == 'swipe_left':
-        pyautogui.press('left')
-    elif gesture == 'swipe_right':
-        pyautogui.press('right')
-    # Add more gestures and corresponding commands as needed
+def perform_action():
+    # Placeholder function for performing actions based on recognized gestures
+    # Implement actions like volume control, opening apps, etc.
+    pass
 
 
 def main():
-    # Your main code goes here
-    print("Hello, World!")
+    # Capture video from the default webcam
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            continue
+
+        # Flip and convert the image to RGB for MediaPipe processing
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        # Draw hand landmarks and check for gestures
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+                if gesture_recognized(hand_landmarks):
+                    perform_action()
+
+        # Display the processed image
+        cv2.imshow('Gesture Link', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
